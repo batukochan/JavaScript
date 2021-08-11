62f45c9630c2880bc0d64f14e6978847d3ef0072
batukochan <batukochan@icloud.com>
2021-08-11 20:50:45 +0300

    calisma

--- 03-Veri Yapıları/dates/calisma/script.js
+++ 03-Veri Yapıları/dates/calisma/script.js
@@ -1,42 +1,54 @@
 // ** Şimdiki tarihin gün ay ve yıl bilgisini yazdırınız.
 
-var dt = new Date();
+// console.log(new Date());
+// console.log(new Date().getDay());
+// console.log(new Date().getMonth());
+// console.log(new Date().getFullYear());
 
+var dt = new Date();
 console.log(dt);
 console.log(dt.getMonth()+1);
-console.log(dt.getDate());
-console.log(dt.getFullYear());
 
 // ** Tarih ve saat bilgisini içeren bir Date objesi oluşturunuz.
-var dtA = new Date('8/24/2010 14:50:10');
-var dtB = new Date(2010,7,24,14,50,10)
 
-console.log(dtA);
-console.log(dtB);
+var dth = new Date('8/24/2021 14:55:12');
+console.log(dth);
 
-// 1/1/1990 tarihinden bir gün öncesini gösteriniz. 
-var dtC = new Date('1/1/1990');
-var dayOfMonth = dtC.getDate();
-dtC.setDate(dayOfMonth-1);
-console.log(dtC);
+// ** */ 1/1/1990 tarihinden bir gün öncesini gösteriniz. 
 
-// iki tarih arasındaki geçen zamanı bulunuz.
-var start = new Date('1/1/1990');
-var end = new Date('2/10/1992');
+var dthA = new Date('1/1/1999');
+var dayOfMounth = dthA.getDate();
+dthA.setDate(dayOfMounth - 1);
+console.log(dthA)
+
+// ** iki tarih arasındaki geçen zamanı bulunuz.
+
+var start = new Date('1/1/1994');
+var end = new Date('1/1/1995');
 
 var milisecond = end - start;
 var saniye = milisecond / 1000;
-var dakika = saniye / 60;
+var dakika = saniye/60;
 var saat = dakika / 60;
 var gun = saat / 24;
 
-console.log('milisecond :'+milisecond)
-console.log('saniye : '+ saniye);
-console.log('dakika :'+ dakika);
-console.log('saat :' + saat);
-console.log('gun : '+gun);
+console.log('milisecond :' +milisecond  )
+console.log('saniye : '+saniye)
+console.log('dakika : '+dakika)
+console.log('saat : '+saat)
+console.log('gün : '+gun)
 
-// Her yıl mayıs ayının 2.haftası pazar günü kutlanan anneler günü 2019 yılında ne zaman kutlanacaktır ?
+// ** Yaş hesaplama nasıl yapılır ?
+
+var birthday = new Date('3/14/1998').getTime();
+var today = Date.now();
+var ageDifMs = today -birthday;
+var ageDate = new Date(ageDifMs);
+var age = ageDate.getUTCFullYear() -1970;
+console.log(age)
+
+
+// ** Her yıl mayıs ayının 2.haftası pazar günü kutlanan anneler günü 2019 yılında ne zaman kutlanacaktır ?
 
 var annelerGunu = new Date();
 annelerGunu.setHours(0,0,0,0);
@@ -44,24 +56,12 @@ annelerGunu.setFullYear(2019);
 annelerGunu.setDate(1);
 annelerGunu.setMonth(4);
 
-while(annelerGunu.getDay() != 0){
+while(annelerGunu.getDay() != 0) {
     annelerGunu.setDate(annelerGunu.getDate()+1)
 }
 annelerGunu.setDate(annelerGunu.getDate()+7);
 console.log(annelerGunu);
 
-// ** Yaş hesaplama nasıl yapılır ?
-
-var birthday = new Date('8/1/1985');
-var ageDifMs = Date.now() - birthday.getTime();
-var ageDate = new Date(ageDifMs);
-
-console.log(ageDate.getFullYear() - 1970);
-// console.log(birthday.getTime());
-// console.log(Date.now())
 
 
-var kurs = "Modern Javascript Dersleri: Baştan Sona Javascript Programlama";
-var result = kurs.lastIndexOf("Javascript"); // result: 7
 
-console.log(result)
